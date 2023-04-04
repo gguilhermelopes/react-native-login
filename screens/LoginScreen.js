c0cb1ac4e13f49d3ccc07a45992a11099077ae80
gguilhermelopes <guilhermelopes275@gmail.com>
2023-04-03 19:47:33 -0700

    feat: set stack on app to load with logged user

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [loading, setLoading] = useState(false);
+  const authContext = useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
     setLoading(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authContext.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not log you in - Please check your credencials or try again later!"
       );
+      setLoading(false);
     }
-    setLoading(false);
   }
 
   if (loading) {
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [loading, setLoading] = useState(false);
+  const authContext = useContext(AuthContext);
 
   async function signUpHandler({ email, password }) {
     setLoading(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authContext.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not create user - Please check your input or try again later!"
       );
+      setLoading(false);
     }
-    setLoading(false);
   }
 
   if (loading) {
