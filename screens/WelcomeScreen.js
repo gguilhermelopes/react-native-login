36f183ff859bc86876bf13e36ac513a1469e50b9
gguilhermelopes <guilhermelopes275@gmail.com>
2023-04-03 20:21:45 -0700

    feat: add rules to backend to allow fetch of database only if logged in (using token)

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState("");
+  const authContext = useContext(AuthContext);
+  const token = authContext.token;
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `https://react-native-course-1a6ac-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      );
+      setMessage(response.data);
+    }
+    fetchData();
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
@@ -14,13 +30,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
